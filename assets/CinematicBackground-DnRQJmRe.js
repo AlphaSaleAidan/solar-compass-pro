91f068f8e58e4bbc53d2b6f210f420cca1f41872
Viktor AI <viktor@alphasale.co>
2026-04-08 13:02:48 +0000

    Fix NTP flow: use persisted fields (documentsSigned, approvalStatus) not lifecycleState

--- assets/CinematicBackground-DnRQJmRe.js
+++ assets/CinematicBackground-DnRQJmRe.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-DEPr-6e1.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-Bzc-tC1e.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
