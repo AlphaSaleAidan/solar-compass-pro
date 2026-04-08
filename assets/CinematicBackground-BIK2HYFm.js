fcd4ac7b1852ee07e3aea17e3a6b03b3460a500e
Viktor AI <viktor@alphasale.co>
2026-04-08 13:11:29 +0000

    Fix: align updateSellProject with actual DB schema (all_docs_signed, dirty_notes)

--- assets/CinematicBackground-BIK2HYFm.js
+++ assets/CinematicBackground-BIK2HYFm.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-Bzc-tC1e.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-DieEYIxT.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
