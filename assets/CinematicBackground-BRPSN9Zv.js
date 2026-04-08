f212ae144caa4a7b2ca3e3c7056dcbdf74347d86
Viktor AI <viktor@alphasale.co>
2026-04-08 12:02:32 +0000

    White-bg prize icons + multiply blend: backgrounds now truly invisible

--- assets/CinematicBackground-BRPSN9Zv.js
+++ assets/CinematicBackground-BRPSN9Zv.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-CR0tvGyM.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-DjdNtrKY.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
