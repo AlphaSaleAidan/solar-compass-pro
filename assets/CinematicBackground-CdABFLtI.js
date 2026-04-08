aedd9474aa74512b8d2ef154618199cca468997d
Viktor AI <viktor@alphasale.co>
2026-04-08 10:41:51 +0000

    Fix handleRefreshRecs - pass as prop to OverviewTab

--- assets/CinematicBackground-CdABFLtI.js
+++ assets/CinematicBackground-CdABFLtI.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-DwytjWTF.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-D0NjXGsJ.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
