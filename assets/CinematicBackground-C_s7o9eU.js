c7f75b5e7f0220a3e8f07b219fce2ce918844d2e
Viktor AI <viktor@alphasale.co>
2026-04-08 06:15:07 +0000

    SupabaseProjectStore rejection actions + parallax hooks + RBAC wired

--- assets/CinematicBackground-C_s7o9eU.js
+++ assets/CinematicBackground-C_s7o9eU.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-C4XuwebZ.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-DDyC7kmK.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
