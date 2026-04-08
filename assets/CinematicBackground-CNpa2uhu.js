299cd0042ee4b315f76d996a6f7168831503e31b
Viktor AI <viktor@alphasale.co>
2026-04-08 05:56:10 +0000

    Add M1-M7 visual timeline to Installer, Financier, and Ops portals

--- assets/CinematicBackground-CNpa2uhu.js
+++ assets/CinematicBackground-CNpa2uhu.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-BFxILLS6.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-4uNxdqJS.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
