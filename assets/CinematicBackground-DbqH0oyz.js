e48b239c60faa300935cb790aa6aae2691df051d
Viktor AI <viktor@alphasale.co>
2026-04-08 08:12:36 +0000

    Deploy: complete emoji purge + Alpha tab rename

--- assets/CinematicBackground-DbqH0oyz.js
+++ assets/CinematicBackground-DbqH0oyz.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-q-12p2U-.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-BXxpT8fk.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
