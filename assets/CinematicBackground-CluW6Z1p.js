26bc8b5a958449b3fa3d88f6b51e43ff796443cf
Viktor AI <viktor@alphasale.co>
2026-04-08 11:34:26 +0000

    3D prize icons: all 23 wheel spin items now show photorealistic 3D renders

--- assets/CinematicBackground-CluW6Z1p.js
+++ assets/CinematicBackground-CluW6Z1p.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-1gt6aYSF.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-R7k--arv.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
