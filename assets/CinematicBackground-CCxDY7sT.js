d454463cd4a124847887fa4f6573712ec667904b
Viktor AI <viktor@alphasale.co>
2026-04-08 08:36:32 +0000

    Council overhaul: rich agent responses, directive consensus, live thoughts on overview

--- assets/CinematicBackground-CCxDY7sT.js
+++ assets/CinematicBackground-CCxDY7sT.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-BXxpT8fk.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-CSc2bF-W.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
