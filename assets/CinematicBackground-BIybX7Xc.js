f0f1e100b9ca1a631f3270b654c92751f010dbf3
Viktor AI <viktor@alphasale.co>
2026-04-08 10:08:00 +0000

    Deploy: in-app messaging system + council dynamic responses

--- assets/CinematicBackground-BIybX7Xc.js
+++ assets/CinematicBackground-BIybX7Xc.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-CIJuxHWM.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-DCEYShhz.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
