62f134348c0317b832d8fc11480c76c2db2a0963
Viktor AI <viktor@alphasale.co>
2026-04-08 06:13:02 +0000

    RBAC skeleton + permission system wired into AuthContext

--- assets/CinematicBackground-DM3r9HxK.js
+++ assets/CinematicBackground-DM3r9HxK.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-4uNxdqJS.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-C4XuwebZ.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
