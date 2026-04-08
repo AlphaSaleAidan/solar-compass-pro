df8d308416a797cf8e40cb5f4382583de8107a38
Viktor AI <viktor@alphasale.co>
2026-04-08 11:49:29 +0000

    Prize icons: mix-blend-mode screen removes black bg + glass-panel contrast boost

--- assets/CinematicBackground-DhQV5M-P.js
+++ assets/CinematicBackground-DhQV5M-P.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-R7k--arv.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-CR0tvGyM.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
