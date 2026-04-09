20d575741df4dfaf85372eef8def58233145a86c
Viktor AI <viktor@alphasale.co>
2026-04-09 22:40:47 +0000

    Deploy: Cascades + skeletons + code splitting (bundle 57% smaller)

--- assets/CinematicBackground-CQsQtT2I.js
+++ assets/CinematicBackground-CQsQtT2I.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-DSu-ZCJo.js";/**
+import{g as xe,S as Dy,k as Pe}from"./index-C50q5YeG.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
