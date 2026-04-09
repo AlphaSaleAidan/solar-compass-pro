23f9aaad915befe0455cd6bd9f9aecfc8fceb70a
Viktor AI <viktor@alphasale.co>
2026-04-09 21:09:13 +0000

    Deploy: Gamification wiring + cascade notifications + portal polish

--- assets/CinematicBackground-Qu11R8VW.js
+++ assets/CinematicBackground-Qu11R8VW.js
@@ -1,4 +1,4 @@
-import{r as xe,g as Dy,j as Pe}from"./index-CNNvNBsR.js";/**
+import{r as xe,g as Dy,j as Pe}from"./index-Bz35qG9V.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
